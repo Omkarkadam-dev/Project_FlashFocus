22b537bdf43b962b1e3a0d399fa2a9eb5a434d6f
Omkar Kadam <omkarkadam1115@gmail.com>
2025-04-10 22:16:24 +0530

    add hero section

---
+++ script.js
@@ -0,0 +1,5 @@
+// Simple animation on page load
+window.addEventListener('load', () => {
+    document.querySelector('.hero-text').classList.add('animate');
+    document.querySelector('.hero-visual').classList.add('animate');
+});

43253089ea1ff8adde400cbacbfd7ddcc55c5e54
Omkar Kadam <omkarkadam1115@gmail.com>
2025-04-10 22:29:31 +0530

    add how it works sections

--- script.js
+++ script.js
@@ -23,3 +23,21 @@ const revealOnScroll = () => {
 window.addEventListener('scroll', revealOnScroll);
 window.addEventListener('load', revealOnScroll);
 
+
+// Reveal steps on scroll
+const steps = document.querySelectorAll('.step-box');
+
+const revealSteps = () => {
+  const triggerBottom = window.innerHeight * 0.85;
+
+  steps.forEach(step => {
+    const top = step.getBoundingClientRect().top;
+    if (top < triggerBottom) {
+      step.classList.add('visible');
+    }
+  });
+};
+
+window.addEventListener('scroll', revealSteps);
+window.addEventListener('load', revealSteps);
+

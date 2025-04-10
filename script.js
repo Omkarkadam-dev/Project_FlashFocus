4eea76850fc11559a3f14afebcf70b61c31c1ee8
Omkar Kadam <omkarkadam1115@gmail.com>
2025-04-10 22:26:30 +0530

    add features cards

--- script.js
+++ script.js
@@ -3,3 +3,23 @@ window.addEventListener('load', () => {
     document.querySelector('.hero-text').classList.add('animate');
     document.querySelector('.hero-visual').classList.add('animate');
 });
+
+
+// Reveal cards on scroll
+const cards = document.querySelectorAll('.feature-card');
+
+const revealOnScroll = () => {
+  const triggerBottom = window.innerHeight * 0.9;
+
+  cards.forEach(card => {
+    const cardTop = card.getBoundingClientRect().top;
+
+    if (cardTop < triggerBottom) {
+      card.classList.add('visible');
+    }
+  });
+};
+
+window.addEventListener('scroll', revealOnScroll);
+window.addEventListener('load', revealOnScroll);
+
